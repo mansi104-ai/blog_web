abaaf3d30c599914b1460816d2b60303fd22f657
mansi104-ai <mkb.kalra@gmail.com>
2024-05-21 17:37:01 +0530

    basic setup done

---
+++ blog/src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function Home(){
+  return(
+    <div className ="container">
+      <h1 className="text-3xl font-bold">Blog website</h1>
+      <p> Some content</p>
+    </div>
+  )
+}
