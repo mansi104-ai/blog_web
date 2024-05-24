d8127891cc4b17b32b0024042a9784e458c20fc7
mansi104-ai <mkb.kalra@gmail.com>
2024-05-24 18:45:03 +0530

    localhost:3000/api/draft/draftMode  working

--- blog/pages/api/draft/draftMode.ts
+++ blog/pages/api/draft/draftMode.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { draftMode } from 'next/headers';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  draftMode().enable();
-  return res.status(200).json({ message: 'Draft mode enabled' });
-}
+  res.writeHead(307,{Location : "/"});
+  res.end();
+};
