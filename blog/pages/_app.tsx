a56bd7691c7c40f4029f96c5c4961f700049cebd
mansi104-ai <mkb.kalra@gmail.com>
2024-05-24 10:57:52 +0530

    api route made for blog

--- blog/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { PreviewProvider} from '../lib/PreviewProvider';
-
-export default function App({ Component, pageProps }) {
-  return (
-    <PreviewProvider token={pageProps.token}>
-      <Component {...pageProps} />
-    </PreviewProvider>
-  );
-}
