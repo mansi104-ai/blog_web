6f2869f47ba7e02aacf57ec50e869437d3ebe5c0
mansi104-ai <mkb.kalra@gmail.com>
2024-05-29 16:42:46 +0530

    frontend to be improved

--- blog/src/app/(user)/post/[slug]/page.tsx
+++ blog/src/app/(user)/post/[slug]/page.tsx
@@ -6,12 +6,27 @@ import urlFor from '../../../../../lib/urlFor';
 import Image from 'next/image.js';
 import { PortableText } from 'next-sanity';
 import { RichTextComponents } from '../../../components/RichTextComponents';
+import type{ Post } from '../../../../../typings';
+// import { Post } from '../../../../../typings';
 
 type Props = {
   params: {
     slug: string;
   };
 };
+
+export async function generateStaticParams() {
+  const query = groq`*[_type == 'post']{
+    slug
+  }`;
+
+  const slugs: Post[] = await client.fetch(query);
+  const slugRoutes = slugs.map((slug) => slug.slug.current);
+
+  return slugRoutes.map((slug) => ({
+    slug,
+  }));
+}
 
 async function Post({ params: { slug } }: Props) {
   const query = groq`
@@ -27,61 +42,7 @@ async function Post({ params: { slug } }: Props) {
 
   return (
     <article className="px-10 pb-20">
-      <section className="space-y-2 border border-[#F7AB0A] text-white">
-        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
-          <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
-            <Image
-              className="object-cover object-center mx-auto"
-              src={urlFor(post.mainImage).url()}
-              alt={post.author.name}
-              fill
-            />
-          </div>
-
-          <section className="p-5 bg-[#F7AB0A] w-full">
-            <div className="flex flex-col md:flex-row justify-between gap-y-5">
-              <div>
-                <h1 className="text-4xl font-extrabold">{post.title}</h1>
-                <p>
-                  {new Date(post._createdAt).toLocaleDateString("en-US", {
-                    day: "numeric",
-                    month: "long",
-                    year: "numeric",
-                  })}
-                </p>
-              </div>
-              <div className="flex items-center space-x-2">
-                <Image
-                  className="rounded-full"
-                  src={urlFor(post.mainImage).url()}
-                  alt={post.author.name}
-                  height={40}
-                  width={40}
-                />
-                <div className="w-64">
-                  <h3 className="text-lg font-bold">{post.author.name}</h3>
-                  <div>{/* {TODO AUTHOR BIO} */}</div>
-                </div>
-              </div>
-              <div>
-                <h2 className="italics pt-10">{post.description}</h2>
-                <div className="flex items-center justify-end mt-auto space-x-2">
-                  {post.categories.map((category: any) => (
-                    <p
-                      key={category._id}
-                      className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4"
-                    >
-                      {category.title}
-                    </p>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-      </section>
-
-      <PortableText value={post.body} components={RichTextComponents} />
+      {/* Rest of your code */}
     </article>
   );
 }
