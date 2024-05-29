894f5093ca305636bd97e5809a60737a08a99f70
mansi104-ai <mkb.kalra@gmail.com>
2024-05-29 13:18:29 +0530

    blog basic completed

--- blog/src/app/(user)/post/[slug]/page.tsx
+++ blog/src/app/(user)/post/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import { groq } from 'next-sanity';
-import { Props } from 'next/script';
+import { ScriptProps as NextScriptProps } from 'next/script';
 import React from 'react';
 import client from '../../../../../lib/sanity.client';
 import urlFor from '../../../../../lib/urlFor';
 import Image from 'next/image.js';
+import { PortableText } from 'next-sanity';
+import { RichTextComponents } from '../../../components/RichTextComponents';
 
 type Props = {
   params: {
@@ -22,46 +24,66 @@ async function Post({ params: { slug } }: Props) {
   `;
 
   const post: any = await client.fetch(query, { slug });
-//   console.log(post);
-  return <article className='px-10 pb-20'>
-    <section className='space-y-2 border border-[#F7AB0A] text-white'>
+
+  return (
+    <article className="px-10 pb-20">
+      <section className="space-y-2 border border-[#F7AB0A] text-white">
         <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
-            <div className = "absolute top-0 w-full h-full opacity-10 blur-sm p-10" >
+          <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+            <Image
+              className="object-cover object-center mx-auto"
+              src={urlFor(post.mainImage).url()}
+              alt={post.author.name}
+              fill
+            />
+          </div>
+
+          <section className="p-5 bg-[#F7AB0A] w-full">
+            <div className="flex flex-col md:flex-row justify-between gap-y-5">
+              <div>
+                <h1 className="text-4xl font-extrabold">{post.title}</h1>
+                <p>
+                  {new Date(post._createdAt).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+              <div className="flex items-center space-x-2">
                 <Image
-                  className = "object-cover object-center mx-auto"
-                  src= {urlFor(post.mainImage).url()}
-                  alt = {post.author.name}
-                  fill
+                  className="rounded-full"
+                  src={urlFor(post.mainImage).url()}
+                  alt={post.author.name}
+                  height={40}
+                  width={40}
                 />
-            </div>
-
-            <section className='p-5 bg-[#F7AB0A] w-full'>
-                <div className='flex flex-col md:flex-row justify-between gap-y-5'>
-                    <div>
-                        <h1 className = "text-4xl font-extrabold">{post.title}
-                            
-                        </h1>
-
-                        <p>
-                        {new Date(post._createdAt).toLocaleDateString(
-                    "en-US",{
-                      day : "numeric",
-                      month : "long",
-                      year : "numeric",
-                    }
-                  )
-                  
-                  }
-                        </p>
-                    </div>
+                <div className="w-64">
+                  <h3 className="text-lg font-bold">{post.author.name}</h3>
+                  <div>{/* {TODO AUTHOR BIO} */}</div>
                 </div>
-
-            </section>
-
+              </div>
+              <div>
+                <h2 className="italics pt-10">{post.description}</h2>
+                <div className="flex items-center justify-end mt-auto space-x-2">
+                  {post.categories.map((category: any) => (
+                    <p
+                      key={category._id}
+                      className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4"
+                    >
+                      {category.title}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
+      </section>
 
-    </section>
-  </article>
+      <PortableText value={post.body} components={RichTextComponents} />
+    </article>
+  );
 }
 
 export default Post;
--- blog/src/app/components/BlogList.tsx
+++ blog/src/app/components/BlogList.tsx
@@ -1,75 +1,64 @@
-// import React from 'react'
 import Image from 'next/image.js';
 import urlFor from '../../../lib/urlFor';
 import type { Post } from '../../../typings.d.ts';
-import category from '../schemaTypes/category';
 import { ArrowUpRightIcon } from '@heroicons/react/24/solid';
 import ClientSide from './ClientSide';
-type Props = {
-  posts : Post[];
 
+type Props = {
+  posts: Post[];
 };
 
-function BlogList({posts}:Props) {
-  // console.log(posts.length);
+function BlogList({ posts }: Props) {
   return (
     <div>
       <hr className='border-[#F7AB0A] mb-10' />
-      <div className='grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 '>
-        {/* {Posts} */}
-        {posts.map(post =>(
-          <ClientSide key = {post._id} route= {`/post/${post.slug.current}`}>
-          <div className='flex flex-col group cursor-pointer'>
-            <div className='relative w-full h-80 drop-shadow-xl rounded-xl group-hover:scale-105 transition-transform duration-200 ease-out'>
-              <Image
-                 className = "object-cover object-left lg:object-center"
-                 src = {urlFor(post.mainImage).url()}
-                 alt = {post.author.name}
-                 fill
-              />
-              <div className='absolute bottom-4 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
-              <div>
-                <p className='font-bold'>{post.title}</p>
-
-                <p>
-                  {new Date(post._createdAt).toLocaleDateString(
-                    "en-US",{
-                      day : "numeric",
-                      month : "long",
-                      year : "numeric",
-                    }
-                  )
-                  
-                  }
-                </p>
-              </div>
-              <div className='flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center'>
-                {post.categories.map(category => (
-                  <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
-                    <p>{category.title}</p>
+      <div className='grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16'>
+        {posts.map((post, index) => (
+          <ClientSide key={index} route={`/post/${post.slug.current}`}>
+            <div className='flex flex-col group cursor-pointer'>
+              <div className='relative w-full h-80 drop-shadow-xl rounded-xl group-hover:scale-105 transition-transform duration-200 ease-out'>
+                {post.mainImage && (
+                  <Image
+                    className="object-cover object-left lg:object-center"
+                    src={urlFor(post.mainImage).url()}
+                    alt={post.author?.name || 'author'}
+                    fill
+                  />
+                )}
+                <div className='absolute bottom-4 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
+                  <div>
+                    <p className='font-bold'>{post.title}</p>
+                    <p>
+                      {new Date(post._createdAt).toLocaleDateString("en-US", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}
+                    </p>
                   </div>
-                ))}
+                  <div className='flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center'>
+                    {post.categories.map((category, index) => (
+                      <div key={index} className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
+                        <p>{category.title}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
+              <div className="mt-5 flex-1">
+                <p className='underline text-lg font-bold'>{post.title}</p>
+                <p className='text-gray-500'>{post.description}</p>
+              </div>
+              <p className='ml-5 font-bold flex items-center group-hover:underline'>
+                Read Post
+                <ArrowUpRightIcon className='ml-2 h-4 w-4' />
+              </p>
             </div>
-          </div>
-
-          <div className = "mt-5 flex-1">
-
-            <p className='underline text-lg font-bold'>{post.title}</p>
-
-            <p className=' text-gray-500'>{post.description}</p>
-
-          </div>
-          <p className='ml-5 font-bold flex items-center group-hover:underline'>
-            Read Post
-            <ArrowUpRightIcon className='ml-2 h-4 w-4' />
-          </p>
-          </div>
           </ClientSide>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default BlogList;
