bb0eb693c811c49f8a0563ee084e1c20fc4d47be
mansi104-ai <mkb.kalra@gmail.com>
2024-05-30 14:09:19 +0530

    production build to be seen

--- blog/src/app/(user)/post/[slug]/page.tsx
+++ blog/src/app/(user)/post/[slug]/page.tsx
@@ -29,6 +29,61 @@ async function Post({ params: { slug } }: Props) {
 
   return (
     <article className="px-10 pb-20">
+      <section className="space-y-2 border border-[#F7AB0A] text-white">
+        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+          <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+            <Image
+              className="object-cover object-center mx-auto"
+              src={urlFor(post.mainImage).url()}
+              alt={post.author.name}
+              fill
+            />
+          </div>
+
+          <section className="p-5 bg-[#F7AB0A] w-full">
+            <div className="flex flex-col md:flex-row justify-between gap-y-5">
+              <div>
+                <h1 className="text-4xl font-extrabold">{post.title}</h1>
+                <p>
+                  {new Date(post._createdAt).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Image
+                  className="rounded-full"
+                  src={urlFor(post.mainImage).url()}
+                  alt={post.author.name}
+                  height={40}
+                  width={40}
+                />
+                <div className="w-64">
+                  <h3 className="text-lg font-bold">{post.author.name}</h3>
+                  <div>{/* {TODO AUTHOR BIO} */}</div>
+                </div>
+              </div>
+              <div>
+                <h2 className="italics pt-10">{post.description}</h2>
+                <div className="flex items-center justify-end mt-auto space-x-2">
+                  {post.categories.map((category: any) => (
+                    <p
+                      key={category._id}
+                      className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4"
+                    >
+                      {category.title}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <PortableText value={post.body} components={RichTextComponents} />
       {/* Rest of your code */}
     </article>
   );
